cf6b6704f5d2eee1153c0f3bf76e01bd2ab3dc0c
TopYun1 <akdzlgkfdkqja@gmail.com>
2022-08-17 23:47:00 +0900

    no message

---
+++ TestClass.java
@@ -0,0 +1,10 @@
+
+public class TestClass {
+	public static void main(String[] args) {
+		boolean result = true;
+		if(0.1+1.1 == 1.2) {
+			result = false;
+		}
+		System.out.println(result);
+	}
+}
